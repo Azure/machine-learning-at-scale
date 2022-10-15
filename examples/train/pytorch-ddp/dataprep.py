23453652ad09a70d437221d6247702e96a1f036e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 07:35:18 +0000

    Adding tarfile member sanitization to extractall()

--- examples/train/pytorch-ddp/dataprep.py
+++ examples/train/pytorch-ddp/dataprep.py
@@ -12,5 +12,24 @@ if not os.path.isdir(data_root):
     os.makedirs(data_root, exist_ok=True)
     urllib.request.urlretrieve(url, filepath)
     with tarfile.open(filepath, "r:gz") as tar:
-        tar.extractall(path=data_root)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=data_root)
     os.remove(filepath)  # delete tar.gz file after extraction
